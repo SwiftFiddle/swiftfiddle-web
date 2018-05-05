80f3b2c73a25c3d0d8808d3b13e93b1ca74cb6ff
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2018-05-05 13:48:23 +0900

    Fix sorting

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ function random(size) {
 function availableVersions() {
   const result = require('child_process').execSync('docker images kishikawakatsumi/swift --format "{{.Tag}}"').toString();
   return result.split('\n').filter(version => version.length > 0).sort((a, b) => {
+    compareVersions = require('compare-versions');
     if (a.includes('.') && b.includes('.')) {
-      return a < b;
+      return compareVersions(a, b) * -1;
     }
     if (a.includes('.')) {
       return 1;
