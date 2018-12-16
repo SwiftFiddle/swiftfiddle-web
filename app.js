31595189751f362190c0bdfef018aa3aa0af58d7
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2018-12-17 00:45:29 +0900

    Support branch name in tag

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ function random(size) {
 
 function availableVersions() {
   const result = require('child_process').execSync('docker images kishikawakatsumi/swift --format "{{.Tag}}"').toString();
-  return result.split('\n').map(version => version.replace('_', '/')).filter(version => version.length > 0).sort((a, b) => {
+  return result.split('\n').filter(version => version.length > 0).sort((a, b) => {
     const compareVersions = require('compare-versions');
-    if (a.includes('/')) {
-      a = a.split('/')[1]
+    if (a.includes('_')) {
+      a = a.split('_')[1]
     }
-    if (b.includes('/')) {
-      b = b.split('/')[1]
+    if (b.includes('_')) {
+      b = b.split('_')[1]
     }
     if (a.includes('.') && b.includes('.')) {
       return compareVersions(a, b) * -1;
