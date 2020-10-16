9d053bd6f70abaec3bef31f76a0b60f9af88dec6
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2020-10-16 15:38:39 +0900

    Fix share image

--- Sources/App/Controllers/ShareImage.swift
+++ Sources/App/Controllers/ShareImage.swift
@@ -4,7 +4,7 @@ struct ShareImage {
     static func image(client: Client, from code: String) throws -> EventLoopFuture<ByteBuffer?> {
         return client.post("http://carbonara:3000/api/cook", headers: ["Content-Type": "application/json"]) {
             try $0.content.encode(
-                ["code": "\(code.split(separator: "\n").prefix(30))",
+                ["code": code.split(separator: "\n").prefix(30).joined(separator: "\n"),
                  "backgroundColor": "rgba(255, 255, 255, 0)",
                  "language": "swift",
                  "paddingHorizontal": "0px",
