a56b03dcc20ffbbf05e755e1d372bb25911ff0ad
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-05-30 22:06:14 +0900

    Work around WebSocket address

--- Public/js/app.js
+++ Public/js/app.js
@@ -53,7 +53,8 @@ function run(editor) {
   const location = window.location;
   const connection = new WebSocket(
     // prettier-ignore
-    `${location.protocol === "https:" ? "wss:" : "ws:"}//${location.host}${location.pathname}ws/${nonce}/run`
+    // `${location.protocol === "https:" ? "wss:" : "ws:"}//${location.host}${location.pathname}ws/${nonce}/run`
+    `ws://35.184.164.96:8080/ws/${nonce}/run`
   );
   connection.onmessage = (e) => {
     const data = JSON.parse(e.data);
