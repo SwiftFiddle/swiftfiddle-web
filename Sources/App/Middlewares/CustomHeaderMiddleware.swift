6f6bc320938b18553f0d932919c8f242e459d9c9
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-06-07 10:27:30 +0900

    Fix Permissions-Policy header

--- Sources/App/Middlewares/CustomHeaderMiddleware.swift
+++ Sources/App/Middlewares/CustomHeaderMiddleware.swift
@@ -2,11 +2,11 @@ import Vapor
 
 final class CustomHeaderMiddleware: Middleware {
     func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
-        return next.respond(to: request).map { response in
+        return next.respond(to: request).map { (response) in
             if !request.url.path.hasSuffix("embedded") && !request.url.path.hasSuffix("embedded/") {
                 response.headers.add(name: "X-Frame-Options", value: "DENY")
-                response.headers.add(name: "Permissions-Policy", value: "interest-cohort=()")
             }
+            response.headers.add(name: "Permissions-Policy", value: "interest-cohort=()")
             return response
         }
     }
