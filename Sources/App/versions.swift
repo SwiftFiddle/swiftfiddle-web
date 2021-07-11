4ed20fad8f26234c18ed56ae5432d6325cadf332
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2021-07-12 03:33:46 +0900

    Make stable version 5.4.2

--- Sources/App/versions.swift
+++ Sources/App/versions.swift
@@ -2,16 +2,13 @@ import Foundation
 import TSCBasic
 
 func latestVersion() throws -> String { try availableVersions()[0] }
-func stableVersion() -> String { "5.4.1" }
+func stableVersion() -> String { "5.4.2" }
 
 func availableVersions() throws -> [String] {
     let process = Process(args: "docker", "images", "--filter=reference=swift", "--filter=reference=*/swift", "--format", "{{.Tag}}")
     try process.launch()
-    try process.waitUntilExit()
-
-    guard let output = try process.result?.utf8Output(), !output.isEmpty else  {
-        return [stableVersion()]
-    }
+    let output = try process.waitUntilExit().utf8Output()
+    guard !output.isEmpty else  { return [stableVersion()] }
 
     let versions = Set(output.split(separator: "\n").map { $0.replacingOccurrences(of: "-bionic", with: "").replacingOccurrences(of: "-focal", with: "").replacingOccurrences(of: "-slim", with: "").replacingOccurrences(of: "snapshot-", with: "") }).sorted(by: >)
     return versions.isEmpty ? [stableVersion()] : versions
@@ -20,11 +17,8 @@ func availableVersions() throws -> [String] {
 func imageTag(for prefix: String) throws -> String? {
     let process = Process(args: "docker", "images", "--filter=reference=swift", "--filter=reference=*/swift", "--format", "{{.Tag}} {{.Repository}}:{{.Tag}}")
     try process.launch()
-    try process.waitUntilExit()
-
-    guard let output = try process.result?.utf8Output(), !output.isEmpty else  {
-        return nil
-    }
+    let output = try process.waitUntilExit().utf8Output()
+    guard !output.isEmpty else  { return nil }
 
     return output
         .split(separator: "\n")
