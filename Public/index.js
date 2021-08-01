935d334f200a6a151eca981b2e047de731dfc3ce
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-02 05:15:22 +0900

    Add Sentry for error reporting

--- Public/index.js
+++ Public/index.js
@@ -1,3 +1,11 @@
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+Sentry.init({
+  dsn: "https://56bc351f77be41d08e91bfefd51ff65c@o938512.ingest.sentry.io/5888290",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 import "./scss/index.scss";
 import "./css/index.css";
 import "./css/version_picker.css";
