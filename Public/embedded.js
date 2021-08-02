53ec40377b95745c71eac7be30dbaaef4b06b122
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-03 00:28:56 +0900

    Fix for embedded content

--- Public/embedded.js
+++ Public/embedded.js
@@ -1,3 +1,23 @@
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+Sentry.init({
+  dsn: "https://56bc351f77be41d08e91bfefd51ff65c@o938512.ingest.sentry.io/5888290",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faCodeBranch,
+  faPlay,
+  faCircleNotch,
+  faExternalLinkAlt,
+} from "@fortawesome/pro-solid-svg-icons";
+
+library.add(faCodeBranch, faPlay, faCircleNotch, faExternalLinkAlt);
+dom.watch();
+
 import "./scss/index.scss";
 import "./css/embedded.css";
 
