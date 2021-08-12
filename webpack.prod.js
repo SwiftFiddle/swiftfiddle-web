4531baef71d7dffc4a69620f63aacee946e92e63
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-13 02:20:49 +0900

    BundleAnalyzerPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerMode: "static", openAnalyzer: false }),
+  ],
 });
