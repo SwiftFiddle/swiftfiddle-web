1f8c18441d427e850fae4c09f171eadf371d0213
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-15 14:53:47 +0900

    Refactor

--- Public/js/encoder.js
+++ Public/js/encoder.js
@@ -2,7 +2,7 @@ import { gzip } from "pako";
 
 export class Encoder {
   static encode(data) {
-    const json = JSON.stringify({ code: data.code, version: data.version });
+    const json = JSON.stringify(data);
     const gziped = gzip(json);
     const base64 = btoa(String.fromCharCode(...gziped));
     return encodeURIComponent(base64);
