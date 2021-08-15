b977ab6374940915c684e651f68f61e67b44e6f5
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-15 09:27:18 +0900

    contenthash.css

--- webpack.common.js
+++ webpack.common.js
@@ -62,7 +62,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "./style.css",
+      filename: "./[name].[contentHash].css",
     }),
     new HtmlWebpackPlugin({
       chunks: ["index"],
