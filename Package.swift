9554921fa94e80ddbc5c40afc26fe4dd81d588a0
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-05-20 17:43:13 +0900

    format

--- Package.swift
+++ Package.swift
@@ -2,30 +2,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-playground",
-    platforms: [
-        .macOS(.v12)
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.2"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Leaf", package: "leaf"),
-            ],
-            swiftSettings: [
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
-            ]
-        ),
-        .testTarget(
-            name: "AppTests", dependencies: [
-                .target(name: "App"),
-                .product(name: "XCTVapor", package: "vapor"),
-            ]
-        )
-    ]
+  name: "swift-playground",
+  platforms: [
+    .macOS(.v12)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.76.2"),
+    .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "Leaf", package: "leaf"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+      ]
+    ),
+    .testTarget(
+      name: "AppTests", dependencies: [
+        .target(name: "App"),
+        .product(name: "XCTVapor", package: "vapor"),
+      ]
+    )
+  ]
 )
--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,41 +2,41 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testRootPath() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
+  func testRootPath() throws {
+    let app = Application(.testing)
+    defer { app.shutdown() }
+    try configure(app)
 
-        try app.test(.GET, "/", afterResponse: { res in
-            XCTAssertEqual(res.status, .ok)
-        })
+    try app.test(.GET, "/", afterResponse: { res in
+      XCTAssertEqual(res.status, .ok)
+    })
+  }
+
+  func testGistPath() throws {
+    let path = "/b4f866efb1c1dc63b0a9cce000cf5688"
+
+    let pattern = try! NSRegularExpression(pattern: #"^\/([a-f0-9]{32})$"#, options: [.caseInsensitive])
+    let matches = pattern.matches(in: path, options: [], range: NSRange(location: 0, length: NSString(string: path).length))
+    guard matches.count == 1 && matches[0].numberOfRanges == 2 else {
+      XCTFail()
+      return
     }
+    let gistId = NSString(string: path).substring(with: matches[0].range(at: 1))
 
-    func testGistPath() throws {
-        let path = "/b4f866efb1c1dc63b0a9cce000cf5688"
+    let ex = expectation(description: "")
 
-        let pattern = try! NSRegularExpression(pattern: #"^\/([a-f0-9]{32})$"#, options: [.caseInsensitive])
-        let matches = pattern.matches(in: path, options: [], range: NSRange(location: 0, length: NSString(string: path).length))
-        guard matches.count == 1 && matches[0].numberOfRanges == 2 else {
-            XCTFail()
-            return
-        }
-        let gistId = NSString(string: path).substring(with: matches[0].range(at: 1))
-
-        let ex = expectation(description: "")
-
-        let session = URLSession(configuration: .default)
-        let request = URLRequest(url: URL(string: "https://api.github.com/gists/\(gistId)")!)
-        session.dataTask(with: request) { (data, response, error) in
-            guard let data = data else {
-                XCTFail()
-                return
-            }
-            if let contents = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-               let files = contents["files"] as? [String: Any],
-               let filename = files.keys.first, let file = files[filename] as? [String: Any],
-               let content = file["content"] as? String {
-                XCTAssertEqual(content, """
+    let session = URLSession(configuration: .default)
+    let request = URLRequest(url: URL(string: "https://api.github.com/gists/\(gistId)")!)
+    session.dataTask(with: request) { (data, response, error) in
+      guard let data = data else {
+        XCTFail()
+        return
+      }
+      if let contents = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+         let files = contents["files"] as? [String: Any],
+         let filename = files.keys.first, let file = files[filename] as? [String: Any],
+         let content = file["content"] as? String {
+        XCTAssertEqual(content, """
                     struct Player {
                         var name: String
                         var highScore: Int = 0
@@ -50,23 +50,23 @@ final class AppTests: XCTestCase {
                     var player = Player("Tomas")
 
                     """)
-            } else {
-                XCTFail()
-            }
+      } else {
+        XCTFail()
+      }
 
-            ex.fulfill()
-        }
-        .resume()
-
-        waitForExpectations(timeout: 5)
+      ex.fulfill()
     }
+    .resume()
 
-    func testShareImage() async throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
+    waitForExpectations(timeout: 5)
+  }
 
-        let data = try await ShareImage.generate(client: app.client, from: "import Foundation")
-        _ = try XCTUnwrap(data)
-    }
+  func testShareImage() async throws {
+    let app = Application(.testing)
+    defer { app.shutdown() }
+    try configure(app)
+
+    let data = try await ShareImage.generate(client: app.client, from: "import Foundation")
+    _ = try XCTUnwrap(data)
+  }
 }
