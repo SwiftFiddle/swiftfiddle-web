af602c123e0e96cd60b2adb2a89304c7429e7fef
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-11-05 17:38:08 +0900

    Revert "test"
    This reverts commit 996464377cec5559dbef299315f2e77cc0b4b4a1.

--- Public/js/runner.js
+++ Public/js/runner.js
@@ -177,16 +177,15 @@ export class Runner {
         }
         result = await reader.read();
       }
-
-      return { stdout: outputs.join("\n"), stderr: errors.join("\n") };
     } catch (error) {
       const isCancel = error.name == "AbortError";
+      completion([], "", error, isCancel);
       if (!isCancel) {
         this.terminal.writeln(`\x1b[37m❌  ${error}\x1b[0m`);
       }
-
-      return { stdout: "", stderr: "" };
     }
+
+    return { stdout: outputs.join("\n"), stderr: errors.join("\n") };
   }
 
   stop() {
