f699a801205ed30800aaf416b5f30f83ac5ab37f
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-04-08 21:57:23 +0900

    LSP

--- Public/js/app.js
+++ Public/js/app.js
@@ -52,7 +52,7 @@ export class App {
     let sequence = 0;
 
     const languageServer = new LanguageServer(
-      "wss://swiftfiddle.com/lang-server/api"
+      "wss://swiftfiddle-lsp.onrender.com/lang-server/api"
     );
 
     languageServer.onconnect = () => {
