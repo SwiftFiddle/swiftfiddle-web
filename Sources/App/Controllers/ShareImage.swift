48692553e7a9d24b8f402c0b9a6ca66566dad502
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-04-10 15:43:27 +0900

    Use freeze for share OGP image

--- Sources/App/Controllers/ShareImage.swift
+++ Sources/App/Controllers/ShareImage.swift
@@ -3,60 +3,33 @@ import Vapor
 struct ShareImage {
   @available(macOS 12.0.0, *)
   static func generate(client: Client, from code: String) async throws -> Data? {
-    let code = code
-      .replacingOccurrences(of: #"\"#, with: #"\\"#)
-      .replacingOccurrences(of: #"""#, with: #"\""#)
-      .split(separator: "\n", omittingEmptySubsequences: false)
-      .prefix(30)
-      .joined(separator: #"\n"#)
-    let state = #"""
-      {
-          "paddingVertical": "0px",
-          "paddingHorizontal": "0px",
-          "backgroundImage": null,
-          "backgroundImageSelection": null,
-          "backgroundMode": "color",
-          "backgroundColor": "rgba(255, 255, 255, 0)",
-          "dropShadow": false,
-          "dropShadowOffsetY": "20px",
-          "dropShadowBlurRadius": "68px",
-          "theme": "one-light",
-          "windowTheme": "none",
-          "language": "swift",
-          "fontFamily": "Hack",
-          "fontSize": "14px",
-          "lineHeight": "133%",
-          "windowControls": true,
-          "widthAdjustment": true,
-          "lineNumbers": false,
-          "firstLineNumber": 1,
-          "exportSize": "2x",
-          "watermark": false,
-          "squaredImage": false,
-          "hiddenCharacters": false,
-          "name": "",
-          "width": 680,
-          "code": "\#(code)"
+    let process = Process()
+    #if os(macOS)
+    process.executableURL = URL(fileURLWithPath: "/opt/homebrew/bin/freeze")
+    #else
+    process.executableURL = URL(fileURLWithPath: "/home/linuxbrew/.linuxbrew/bin/freeze")
+    #endif
+    process.arguments = ["--language", "swift"]
+
+    let standardInput = Pipe()
+    process.standardInput = standardInput
+    process.standardOutput = Pipe()
+
+    try standardInput.fileHandleForWriting.write(contentsOf: Data(code.utf8))
+    try standardInput.fileHandleForWriting.close()
+
+    _ = try await withCheckedThrowingContinuation { (continuation) in
+      process.terminationHandler = { (process) in
+        continuation.resume(returning: process.terminationStatus)
       }
-      """#
-      .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)?
-      .data(using: .utf8)?
-      .base64EncodedString()
+      do {
+        try process.run()
+      } catch {
+        continuation.resume(throwing: error)
+      }
+    }
 
-    var request = ClientRequest(
-      method: .POST,
-      url: "https://carbon.now.sh/api/image",
-      headers: ["Origin": "https://swiftfiddle.com"]
-    )
-    try request.content.encode(["state": state], as: .json)
-    let response = try await client.send(request)
-
-    guard let body = response.body, let string = body.getString(at: 0, length: body.readableBytes, encoding: .utf8) else { return nil }
-
-    let base64Encoded = string.replacingOccurrences( of: "data:image/png;base64,", with: "")
-    guard let data = Data(base64Encoded: base64Encoded, options: []) else { return nil }
-
-    return data
+    return try Data(contentsOf: URL(fileURLWithPath: "freeze.png"))
   }
 }
 
